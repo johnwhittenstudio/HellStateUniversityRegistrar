5b4b2348f90bf6a196b6d705de85472cb466f58e
John Whitten <johnwhitten.studio@gmail.com>
2022-03-14 21:00:25 -0700

    update at working app point Co-authored-by: Erin McCulley <ejmcculley@gmail.com>

---
+++ UniversityRegistrar/Models/Student.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace UniversityRegistrar.Models
+{
+  public class Student
+  {
+    public Student()
+    {
+      this.JoinEntities = new HashSet<CourseStudent>();
+    }
+
+    public int StudentId { get; set; }
+    public string Name { get; set; }
+    public string EnrollmentDate { get; set; }
+
+    public virtual ICollection<CourseStudent> JoinEntities { get;}
+  }
+}
